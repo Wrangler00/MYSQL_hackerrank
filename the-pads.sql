4ba51166537f65a23660333078a1769b27ae6979
Abhishek Kumar <abhishekwrangler@gmail.com>
2020-12-13 01:12:00 +0530

    Create the-pads.sql
    https://www.hackerrank.com/challenges/the-pads/problem

---
+++ the-pads.sql
@@ -0,0 +1,10 @@
+// link: https://www.hackerrank.com/challenges/the-pads/problem
+
+select concat(o.Name,'(',LEFT(o.Occupation,1),')') as nm
+from occupations as o
+order by nm;
+
+select CONCAT('There are a total of ',COUNT(*),' ',LOWER(o.Occupation),'s.') as sen
+from occupations as o
+group by o.Occupation
+order by sen;
