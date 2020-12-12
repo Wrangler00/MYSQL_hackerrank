4322d3c40a5eb24e32443ebfd7fe681240b9e6ab
Abhishek Kumar <abhishekwrangler@gmail.com>
2020-12-13 01:14:48 +0530

    Update the-pads.sql

--- the-pads.sql
+++ the-pads.sql
@@ -8,3 +8,15 @@ select CONCAT('There are a total of ',COUNT(*),' ',LOWER(o.Occupation),'s.') as 
 from occupations as o
 group by o.Occupation
 order by sen;
+
+
+OR
+
+select concat(o.Name,'(',LEFT(o.Occupation,1),')') as nm
+from occupations as o
+order by nm;
+
+select CONCAT('There are a total of ',COUNT(1),' ',LOWER(o.Occupation),'s.') as sen
+from occupations as o
+group by o.Occupation
+order by COUNT(1),o.Occupation;
